27c60c5e33d465017d29396dc230c4edbae716ad
Jashan32 <jashandeep1849@gmail.com>
2025-07-15 23:43:46 +0530

    added api server

--- apps/web/src/sidebar.tsx
+++ apps/web/src/sidebar.tsx
@@ -1,17 +1,26 @@
-'use client';
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function Sidebar() {
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(localStorage.getItem('isOpen') == 'true');
-    console.log("Sidebar isOpen:", isOpen);
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') == 'true');
+    if(!isLoggedIn){
+        navigate("/login")
+    }
     return (
         <div className={` text-white transition-[width] duration-300 ${isOpen ? " w-[232px]" : "w-[68px]"} h-[100vh] m-[8px] mr-0`}>
             <h2 className="text-lg font-bold" onClick={() => { setIsOpen(!isOpen); localStorage.setItem('isOpen', `${!isOpen}`) }} >Sidebar</h2>
-            <ul className="mt-4 space-y-2">
-                <li><a href="/dashboard/home" className="hover:underline">Home</a></li>
-                <li><a href="/dashboard/project" className="hover:underline">Projects</a></li>
-                <li><a href="/dashboard/schedule" className="hover:underline">Schedule</a></li>
-            </ul>
+            <div>
+                <div className="mt-4 space-y-2">
+                    <div onClick={() => navigate("/dashboard/home")} className="hover:underline">Home</div>
+                    <div onClick={() => navigate("/dashboard/project")} className="hover:underline">Projects</div>
+                    <div onClick={() => navigate("/dashboard/schedule")} className="hover:underline">Schedule</div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 }
